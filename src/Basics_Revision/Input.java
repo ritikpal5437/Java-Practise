2b2ae062ae5d93ec75de4f8347eb660ad005b792
Ritik Pal <ritikpal5437@gmail.com>
2026-09-17 18:09:02 +0530

    done

---
+++ src/Basics_Revision/Input.java
@@ -0,0 +1,10 @@
+package Basics_Revision;
+import java.util.Scanner;
+public class Input {
+    public static void main(String[] args) {
+        String name;
+        Scanner sc = new Scanner(System.in);
+        name = sc.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
