84239884bced482a8a0b89c0de9851d08f0b9899
Ritik Pal <ritikpal5437@gmail.com>
2026-09-19 22:08:24 +0530

    finding a numbr

---
+++ src/Array/Countere.java
@@ -0,0 +1,28 @@
+package Array;
+import java.util.Scanner;
+public class Countere {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of an array:");
+        int size=sc.nextInt();
+        System.out.println("Elements are:");
+        int[] arr=new int[size];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]= sc.nextInt();
+
+        }
+        int even=0;
+        int odd=0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] %2==0 ) {
+                even++;
+            }
+            else {
+                odd++;
+            }
+        }
+        System.out.println(even);
+        System.out.println(odd);
+
+    }
+}
