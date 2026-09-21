0d45d7bba80ed378ab27c1a431bc44ee7b5c0b17
Ritik Pal <ritikpal5437@gmail.com>
2026-09-21 22:39:47 +0530

    Fix output formatting in Loops.java to print elements on the same line

--- src/Array/Loops.java
+++ src/Array/Loops.java
@@ -12,10 +12,10 @@ public class Loops {
         }
         System.out.println("Elements are:");
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.print(arr[i]+"");
 
        
        
-       r }
+       }
     }
 }
