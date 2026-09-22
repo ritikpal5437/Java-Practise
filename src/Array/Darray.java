b14280708f6ee60163e81e8820d61008dd6cac5a
Ritik Pal <ritikpal5437@gmail.com>
2026-09-22 22:47:55 +0530

    array

---
+++ src/Array/Darray.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+class Array2D {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter rows:");
+        int rows = sc.nextInt();
+
+        System.out.println("Enter columns:");
+        int col = sc.nextInt();
+
+        int[][] arr = new int[rows][col];
+
+        // Input
+        System.out.println("Enter elements:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+    
+        System.out.println("Array:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
