a300afe50a4221085298dc8386bbfbd3e23c232e
Ritik Pal <ritikpal5437@gmail.com>
2026-09-23 18:41:15 +0530

    Update Linearsearch.java: Change target value to 555 and improve output formatting; add new more.java file

--- src/LinearSearch/Linearsearch.java
+++ src/LinearSearch/Linearsearch.java
@@ -4,10 +4,10 @@ public class Linearsearch {
     public static void main(String[] args) {
 
         int[] arr={345,324,56,78,23,45,78,21,67,23,5};
-        int target=5;
+        int target=555;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == target) {
-                System.out.println("Element"+arr[i]+ " is found at index: " + i);
+                System.out.println("Element: "+ arr[i]+ " is found at index: " + i);
 
                 return;
             }
---
+++ src/LinearSearch/more.java
@@ -0,0 +1,7 @@
+package LinearSearch;
+
+class more{
+    public static void main(String[] args) {
+        
+    }
+}
