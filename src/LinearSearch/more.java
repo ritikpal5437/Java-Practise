44f8096e94808258cfe9db5537e155387fb3d872
Ritik Pal <ritikpal5437@gmail.com>
2026-09-23 18:47:34 +0530

    Refactor more.java: Improve formatting and enhance element search output

--- src/LinearSearch/more.java
+++ src/LinearSearch/more.java
@@ -1,7 +1,25 @@
-package LinearSearch;
-
-class more{
+class more {
     public static void main(String[] args) {
-        
+
+        int[] arr = {123, 123, 123, 3557, 78, 34, 78, 43, 78, 5};
+        int tar = 123;
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == tar) {
+                count++;
+                System.out.println(
+                    "Element: " + arr[i] +
+                    " found at index: " + i
+                );
+            }
+        }
+
+        if (count > 0) {
+            System.out.println("Element found " + count + " times");
+        } else {
+            System.out.println("Element not found");
+        }
     }
 }
