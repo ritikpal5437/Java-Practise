00324976a3669ed630151cacc6914da4d7625bed
Ritik Pal <ritikpal5437@gmail.com>
2026-09-24 17:25:17 +0530

    Refactor binary search implementation: correct target value, fix syntax errors, and improve code readability

--- src/BinarySearch/intro.java
+++ src/BinarySearch/intro.java
@@ -1,5 +1,4 @@
 
-import javax.swing.text.html.HTML;
 
 /**
  * intro for binary search takking an ex for better understanding
@@ -9,29 +8,35 @@ public class intro {
     public static void main(String[] args) {
 
         int[] arr={-12,-9,-5,0,1,2,4,6,7,8,9,10,11,33,55,77,88,99,123,134,566,788};
-        int target=566;
+        int target=6;
         int ans = binarysearch(arr,target);
         System.out.println(ans);
         
 
-        static int binarysearch(int[] arr,int target){
+    }
+
+    static int binarysearch(int[] arr, int target) {
          int start=0;
          int end = arr.length-1;
 
-         while (start<=.end) {
+         while (start<=end) {
 //find midd elem
 //int mid=(start+end)/2;  but we r not using it because its nt for biiger values of array likes array of million elem
 int mid=start+(end-start)/2;
 if(target<arr[mid]){
     end=mid-1;
 }
-else if(target>mid){
+else if(target>arr[mid]){
     start=mid+1;
 }
 else{
     return mid;
 }
-} 
+         }
+
+         return -1;
+
+
              
          }
 
@@ -39,3 +44,15 @@ else{
 
 
     }
+
+
+   /* if (target < arr[mid]) {
+    end = mid - 1;
+}
+else if (target > arr[mid]) {
+    start = mid + 1;
+}
+else {
+    return mid;
+}*/
+
