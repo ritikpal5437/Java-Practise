72f3b3d5a0f54c4cc96c3c8553370a37210cf695
Ritik Pal <ritikpal5437@gmail.com>
2026-09-25 17:59:43 +0530

    ceiling fn for binary serach ques eith

---
+++ src/BinarySearch/ceilingLeetcodeQue.java
@@ -0,0 +1,36 @@
+package BinarySearch;
+
+public class ceilingLeetcodeQue {
+        public static void main(String[] args) {
+            int[] arr={-12,-9,-5,0,1,2,4,6,7,8,9,10,11,33,55,77,88,99,123,134,566,788};
+            int target=5;
+            int ans = ceiling(arr,target);
+            System.out.println(ans);
+
+        }
+
+        static int ceiling(int[] arr, int target) {
+            int start = 0;
+            int end = arr.length - 1;
+
+            while (start <= end) {
+                int mid = start + (end - start) / 2;
+                if (target < arr[mid]) {
+                    end = mid - 1;
+                } else if (target > arr[mid]) {
+                    start = mid + 1;
+                } else {
+                    return mid;
+                }
+            }
+
+            return start;
+
+
+        }
+
+
+    }
+
+
+
